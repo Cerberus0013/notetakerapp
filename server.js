d4bb5743e3d2a8e1bc4a4a1fac7a22672934da33
Breedlove13 <b.breedlove@outlook.com>
2021-04-15 19:45:36 -0700

    file structure set up

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//adding express middleware
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello ",
+  });
+});
+
+//default response to user for requets not found
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
