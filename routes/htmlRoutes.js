a831d21dd19ef3315f27ead8c9d1323b60cdff87
Breedlove13 <b.breedlove@outlook.com>
2021-04-16 21:00:02 -0700

    all catch all route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,6 +11,11 @@ route.get("/", (req, res) => {
 
 route.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
+
 });
+
+route.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 module.exports = route;
