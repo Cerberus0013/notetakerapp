3da2263bc09c1bb3fff46f28fab9b936402ccfee
Breedlove13 <b.breedlove@outlook.com>
2021-04-16 19:13:22 -0700

    putting in get and post routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
+const { notes } =require('./db/db.json')
 const app = express();
 
 //adding express middleware
@@ -7,11 +8,43 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello ",
-  });
+
+function findbyId(id, notesArray) {
+  const result = notesArray.filter((note) => note.id === id)[0];
+  return result;
+}
+
+app.get("/api/notes:id", (req, res) => {
+  const result = findbyId(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
+
+function createNewAnimal(body, notesArray) {
+  const animal = body;
+  notesArray.push(animal);
+  //wriying the info to the db
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  //return finished code to post to route
+
+  return note;
+}
+
+app.post("./api/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  console.log(req.body);
+
+  const note = createNewAnimal(req.body, notes);
+  res.json(req.body);
+});
+
+
 
 //default response to user for requets not found
 
